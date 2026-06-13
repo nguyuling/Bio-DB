8c9592407cb74f1b3391acaec3434278693294e8
Yuling👩🏻‍💻 <nguyuling@graduate.utm.my>
2026-06-13 19:03:01 +0800

    done q1 le3

---
+++ LE3-API/q1.py
@@ -0,0 +1,81 @@
+"""
+    Name: NGU YU LING
+    Matric No.: A23CS0149
+    Question 1: List Samples with Filtering and Pagination  
+"""
+
+#! connect to mongodb
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["metagenomics"]
+collection = db["genomes"]
+
+#! create fastapi app instance
+from fastapi import FastAPI, HTTPException, Query
+app = FastAPI(
+    title="Metagenomics API",
+    description="API to for genomes.",
+    version="1.0.0"
+)
+
+#! query parameters
+@app.get("/samples")
+async def list_samples(
+    page: int | None = Query(
+        default = 1,
+        description = "Page number (1-indexed)"
+    ),
+    limit: int | None = Query(
+        default = 20,
+        description = "Number of results per page (max 100)"
+    ),
+    environment_type: str | None = Query (
+        default = None,
+        description = "Filter by Soil, Water, Air, or Sediment"
+    )
+):
+    
+    # documentation
+    """
+    List Samples with Filtering and Pagination.
+    """
+    
+    #! filter
+    match_filter = {}
+    if environment_type:
+        match_filter["environment_type"] = environment_type
+        
+    #! mongodb aggregation pipeline
+    pipeline = [
+        { "$match": match_filter },
+        { "$skip": (page-1) * limit },
+        { "$limit": limit },
+        { "$project": {
+                "_id": 0,
+                "sample_id": 1,
+                "location": 1,
+                "collection_date": 1,
+                "environment_type": 1,
+                "sequence_count": { "$size": "$sequences" }
+            }
+        }
+    ]
+    
+    #! read user input
+    try:
+        cursor = collection.aggregate(pipeline)
+        results = await cursor.to_list(length=limit)        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    #! return output
+    return {
+        "total": await collection.count_documents({}),
+        "page": page,
+        "limit": limit,
+        "data": results
+    }
