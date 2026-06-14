e91f603c9cf2196b623ce448d6c7ad3b5da27f5e
Yuling👩🏻‍💻 <nguyuling@graduate.utm.my>
2026-06-14 21:24:42 +0800

    done q7 le3

--- LE3-API/q7.py
+++ LE3-API/q7.py
@@ -1,7 +1,7 @@
 """
     Name: NGU YU LING
     Matric No.: A23CS0149
-    Question 6a: List and Filter Taxonomy Records    
+    Question 7: Species Diversity per Environment Type    
 """
 
 #! connect to mongodb
@@ -21,3 +21,55 @@ app = FastAPI(
     version="1.0.0"
 )
 
+#! query parameters
+@app.get("/analytics/diversity")
+async def species_diversity(
+    environment_type: str | None = Query(
+        default=None,
+        description="Filter results by an environment type (e.g., Soil, Water)"
+    )
+):
+    """
+    Species Diversity per Environment Type
+    """
+    
+    #! mongodb aggregation pipeline
+    pipeline = []
+    
+    if environment_type:
+        environment_type = environment_type.capitalize()
+        pipeline.append({
+            "$match": { "environment_type": environment_type }
+        })
+        
+    pipeline.extend([
+        { "$unwind": "$sequences" },
+        { "$group": {
+                "_id": "$environment_type",
+                "distinct_species": { "$addToSet": "$sequences.taxonomy.species" }
+            }
+        },
+        { "$project": {
+                "_id": 0,
+                "environment_type": "$_id",
+                "unique_species_count": { "$size": "$distinct_species" }
+            }
+        }
+    ])
+    
+    #! execute pipeline
+    try:
+        cursor = collection.aggregate(pipeline)
+        results = await cursor.to_list(length=10)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+        
+    #! return block with ordered field output format
+    return {
+        "data": [{
+                "environment_type": result.get("environment_type"),
+                "unique_species_count": result.get("unique_species_count")
+            }
+            for result in results
+        ]
+    }
